c6905db88e772f2d3979954d4a1c7bb19347ee73
rhf <1028623847@qq.com>
2020-12-11 15:26:44 +0800

    添加产品的页面，修改路由地址，注释eslint语法

---
+++ src/views/modules/product/category.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <el-tree :data="menus" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        menus: [],
+        defaultProps: {
+          children: 'children',
+          label: 'name'
+        }
+      }
+    },
+    created () {
+      this.handleNodeClick()
+    },
+    methods: {
+      handleNodeClick (data) {
+        this.$http({
+          url: this.$http.adornUrl('/product/category/list/tree'),
+          method: 'get'
+        }).then(({ data }) => {
+          console.log('成功获取到菜单数据...', data.list)
+          this.menus = data.list
+        })
+
+      }
+    }
+  }
+</script>
+
