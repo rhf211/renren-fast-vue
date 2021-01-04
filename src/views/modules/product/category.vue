44e7f934296f1ba115ab147052c602a0aaeaa964
rhf <1028623847@qq.com>
2021-01-04 17:06:52 +0800

    增加品牌模块和上传组件

--- src/views/modules/product/category.vue
+++ src/views/modules/product/category.vue
@@ -3,6 +3,7 @@
     <el-tree :data="menus" :props="defaultProps" @node-click="handleNodeClick"
              show-checkbox
              node-key="catId"
+             draggable
              :expand-on-click-node="false"
              :default-expanded-keys="expandkey">
        <span class="custom-tree-node" slot-scope="{ node, data }">
@@ -87,7 +88,6 @@
           url: this.$http.adornUrl('/product/category/list/tree'),
           method: 'get'
         }).then(({data}) => {
-          console.log('成功获取到菜单数据...', data.list)
           this.menus = data.list
         })
 
@@ -197,6 +197,7 @@
           })
         })
       },
+
     }
   }
 </script>
