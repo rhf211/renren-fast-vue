fdf85b0e2326a274e29a52e4d845e29adb46e67d
rhf211 <10286238472@qq>
2021-01-10 22:53:08 +0800

    删掉多余的字符

--- src/views/modules/product/spuadd.vue
+++ src/views/modules/product/spuadd.vue
@@ -458,13 +458,13 @@ export default {
         brandId: "",
         weight: "",
         publishStatus: 0,
-        decript: [], 
-        images: [], 
+        decript: [],
+        images: [],
         bounds: {
           buyBounds: 0,
           growBounds: 0
         },
-        baseAttrs: [], 
+        baseAttrs: [],
         skus: []
       };
     },
@@ -803,5 +803,5 @@ export default {
   activated() {} //如果页面有keep-alive缓存功能，这个函数会触发
 };
 </script>
-<style scoped>
+<style scoped>
 </style>
